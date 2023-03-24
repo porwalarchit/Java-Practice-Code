b73fa19ae3cab49e266d7536d78e0f4647322964
porwalarchit <architporwal@gmail.com>
2023-03-24 14:28:28 +0530

    Thread Interrupts Examples

---
+++ Multithreading/src/Thread_Interrupts/JavaInterruptExp1.java
@@ -0,0 +1,20 @@
+package Thread_Interrupts;
+public class JavaInterruptExp1 extends Thread
+{
+    public void run() {
+        try {
+            Thread.sleep(1000);
+            System.out.println("javatpoint");
+        }catch(InterruptedException e){
+            throw new RuntimeException("Thread interrupted..."+e);
+
+        }
+    }
+    public static void main(String args[]) {
+        JavaInterruptExp1 t1=new JavaInterruptExp1();
+        t1.start();
+        try {
+            t1.interrupt();
+        }catch(Exception e){System.out.println("Exception handled "+e);}
+    }
+}
